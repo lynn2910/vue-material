4757f41eee43ba684b355e9d33e6202747c00174
lynn2910 <cedric.colin35@gmail.com>
2025-07-12 01:54:10 +0200

    feat(badges): Add small and large badges

---
+++ src/components/material/badges/LargeBadge.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-error rounded-full h-5.5 min-w-5.5 w-fit p-0.5">
+    <p class="text-on-error text-body-small font-bold" style="font-size: 11px">{{ showed }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps<{
+  n: number
+}>();
+
+const showed = computed(() => {
+  if (props.n > 999) return '999+'
+  else return props.n;
+})
+</script>
---
+++ src/components/material/badges/SmallBadge.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-error rounded-full w-1 h-1"></div>
+</template>
