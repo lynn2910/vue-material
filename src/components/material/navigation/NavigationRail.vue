743e7dd55aefaf0c6c752a7771c741c408c78cec
lynn2910 <cedric.colin35@gmail.com>
2025-07-12 00:40:50 +0200

    feat(NavigationRail): Add accessibility

--- src/components/material/navigation/NavigationRail.vue
+++ src/components/material/navigation/NavigationRail.vue
@@ -1,63 +1,74 @@
 <template>
-  <div class="flex flex-col bg-surface mx-3 m"
+  <nav class="flex flex-col bg-surface mx-3 m"
+       aria-label="Navigation"
        :class="{
           'w-26': !isExpanded,
           'min-w-56 max-w-[360px]': isExpanded,
         }">
-    <div class="select-none cursor-pointer text-2xl ml-10 pt-7 pb-5" @click="toggleExpanded">
+    <button type="button" class="select-none cursor-pointer text-2xl ml-10 pt-7 pb-5 w-fit"
+            :aria-expanded="isExpanded"
+            @click="toggleExpanded">
       <i v-if="!hideMenuButton && !isExpanded" class="material-icons-outlined">menu</i>
       <i v-else-if="isExpanded" class="material-icons-outlined">menu_open</i>
-    </div>
+    </button>
 
     <!-- FAB -->
-    <div v-if="fab"
-         class="flex flex-row mt-2 mb-7"
-         :class=" {
-            'bg-primary-container rounded-lg text-on-primary-container w-fit gap-2 ml-6 py-4 pb-2.5 px-4': isExpanded
-         }"
+    <button v-if="fab" type="button"
+            :aria-label="fab.label"
+            class="flex flex-row mt-2 mb-7"
+            :class=" {
+              'bg-primary-container rounded-lg text-on-primary-container w-fit gap-2 ml-6 py-4 pb-2.5 px-4': isExpanded
+            }"
     >
       <div :class="{'mx-auto w-fit p-4 pb-2.5 bg-primary-container rounded-lg': !isExpanded,}">
-        <i class="material-icons-outlined text-2xl text-on-primary-container">{{ fab.icon }}</i>
+        <i aria-hidden="true"
+           class="material-icons-outlined text-2xl text-on-primary-container">{{ fab.icon }}</i>
       </div>
       <p v-if="isExpanded">{{ fab.label }}</p>
-    </div>
+    </button>
 
     <!-- Items -->
-    <div v-for="(item, index) in items" :key="index"
-         class="relative mt-1">
+    <ul>
+      <li v-for="(item, index) in items" :key="index"
+          class="relative mt-1 focus:outline">
 
-      <div v-if="item.type === 'section'">
-        <p class="text-secondary mt-9 mb-5 w-fit pl-2">{{ item.label }}</p>
-      </div>
-      <div v-else
-           class="flex items-center content-center cursor-pointer w-fit gap-2 mb-1.5"
-           :class="{
-             'flex-row ml-5 py-4 px-5': isExpanded,
-             'bg-secondary-container rounded-full text-on-secondary-container': isExpanded && item.selected,
-             'flex-col mx-auto': !isExpanded,
-           }">
-
-        <i class="material-icons-outlined text-center select-none text-2xl"
-           :class="{
-           'text-on-secondary-container': item.selected,
-           'text-on-surface-variant': !item.selected,
-           'bg-secondary-container rounded-full': item.selected && !isExpanded,
-           'w-7/12 py-2': !isExpanded,
-          }">
-          {{ item.icon }}
-        </i>
-        <p class="text-secondary">{{ item.label }}</p>
-
-
-        <!-- badge -->
-        <div v-if="item.show_badge" class="absolute top-0 right-0 bg-error rounded-full">
-        <span v-if="item.badge_type === 'large'"
-              class="bg-on-error">{{ item.badge_label }}</span>
+        <div v-if="item.type === 'section'">
+          <p class="text-secondary mt-9 mb-5 w-fit pl-2">{{ item.label }}</p>
         </div>
-      </div>
-    </div>
+        <button v-else type="button"
+                @click="item.onClick || (() => {})"
+                :aria-selected="item.selected"
+                class="flex items-center content-center cursor-pointer gap-2 mb-1.5 group"
+                :class="{
+                  'flex-row ml-5 py-4 px-5 w-fit': isExpanded,
+                  'bg-secondary-container rounded-full text-on-secondary-container': isExpanded && item.selected,
+                  'flex-col mx-auto w-full': !isExpanded,
+                }">
 
-  </div>
+          <i aria-hidden="true"
+             class="material-icons-outlined text-center select-none text-2xl rounded-full"
+             :class="{
+               'group-hover:bg-secondary-container/50 transition duration-short4': !isExpanded && !item.selected,
+               'text-on-secondary-container': item.selected,
+               'text-on-surface-variant': !item.selected,
+               'bg-secondary-container': item.selected && !isExpanded,
+               'w-8/12 py-4': !isExpanded,
+              }">
+            {{ item.icon }}
+          </i>
+          <p class="text-secondary">{{ item.label }}</p>
+
+
+          <!-- badge -->
+          <div v-if="item.show_badge" class="absolute top-0 right-0 bg-error rounded-full">
+            <span v-if="item.badge_type === 'large'" class="bg-on-error">
+              {{ item.badge_label }}
+            </span>
+          </div>
+        </button>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup lang="ts">
