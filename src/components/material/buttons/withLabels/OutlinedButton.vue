117be8e796ebfb4c327366a409a860ea37bdff0c
lynn2910 <cedric.colin35@gmail.com>
2025-07-12 13:34:40 +0200

    feat(buttons): Add Outlined, Text and Tonal buttons

---
+++ src/components/material/buttons/withLabels/OutlinedButton.vue
@@ -0,0 +1,61 @@
+<template>
+  <button
+    @click="clicked()"
+    :disabled="props.disabled"
+    class="btn_after relative flex flex-row items-center justify-center gap-x-2 py-2.5 px-6 rounded-[6.25em] shadow-lg text-sm tracking-[.00714em] font-medium select-none border border-on-surface-variant/70"
+    :class="{
+      'text-primary cursor-pointer': !props.disabled,
+      'text-on-surface-variant/70 cursor-default': props.disabled
+    }">
+    <i v-if="props.icon" class="  material-icons-outlined">{{ props.icon }}</i>
+    {{ props.label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label: string,
+  icon?: string,
+  disabled?: boolean
+}>();
+
+const emit = defineEmits(['click'])
+
+function clicked() {
+  if (props.disabled) return;
+  emit('click');
+}
+</script>
+
+<style scoped>
+@reference "@/styles/index.css"
+
+.btn_after {
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3), 0 1px 3px 1px rgba(0, 0, 0, 0.15);
+}
+
+.btn_after:not(:disabled)::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-radius: inherit;
+  opacity: 0;
+  transition: opacity 0.2s ease;
+  pointer-events: none;
+}
+
+.btn_after:not(:disabled):hover::after {
+  opacity: 1;
+  @apply bg-primary/10;
+}
+
+.btn_after:not(:disabled):focus::after {
+  opacity: 1;
+  @apply bg-primary/20;
+}
+</style>
+
+
--- src/views/components/ButtonsPresentation.vue
+++ src/views/components/ButtonsPresentation.vue
@@ -11,13 +11,28 @@
       class="flex flex-col gap-5 p-3 bg-surface-container-lowest rounded-lg border border-surface-container-highest">
       <div class="flex flex-row gap-5">
         <ElevatedButton label="Elevated"/>
-        <ElevatedButton label="Elevated" icon="water_drop"/>
+        <ElevatedButton label="Elevated" icon="add"/>
         <ElevatedButton label="Elevated" disabled/>
       </div>
       <div class="flex flex-row gap-5">
-        <FilledButton label="Elevated"/>
-        <FilledButton label="Elevated" icon="water_drop"/>
-        <FilledButton label="Elevated" disabled/>
+        <FilledButton label="Filled"/>
+        <FilledButton label="Filled" icon="add"/>
+        <FilledButton label="Filled" disabled/>
+      </div>
+      <div class="flex flex-row gap-5">
+        <TonalButton label="Tonal"/>
+        <TonalButton label="Tonal" icon="add"/>
+        <TonalButton label="Tonal" disabled/>
+      </div>
+      <div class="flex flex-row gap-5">
+        <OutlinedButton label="Outlined"/>
+        <OutlinedButton label="Outlined" icon="add"/>
+        <OutlinedButton label="Outlined" disabled/>
+      </div>
+      <div class="flex flex-row gap-5">
+        <TextButton label="Text"/>
+        <TextButton label="Text" icon="add"/>
+        <TextButton label="Text" disabled/>
       </div>
     </div>
 
@@ -25,6 +40,9 @@
 </template>
 
 <script setup lang="ts">
-import ElevatedButton from "@/components/material/buttons/ElevatedButton.vue";
-import FilledButton from "@/components/material/buttons/FilledButton.vue";
+import ElevatedButton from "@/components/material/buttons/withLabels/ElevatedButton.vue";
+import FilledButton from "@/components/material/buttons/withLabels/FilledButton.vue";
+import TonalButton from "@/components/material/buttons/withLabels/TonalButton.vue";
+import OutlinedButton from "@/components/material/buttons/withLabels/OutlinedButton.vue";
+import TextButton from "@/components/material/buttons/withLabels/TextButton.vue";
 </script>
