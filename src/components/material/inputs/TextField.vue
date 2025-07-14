140eab967071d1cbf47dcd8d3b12d2af1c2a8b7c
lynn2910 <cedric.colin35@gmail.com>
2025-07-14 19:56:41 +0200

    feat(TextFields): Add control for leading icons

--- src/components/material/inputs/TextField.vue
+++ src/components/material/inputs/TextField.vue
@@ -2,30 +2,28 @@
   <div class="relative flex flex-col">
 
     <div class="relative z-0 bg-surface">
+
+      <div class="absolute left-4 top-4 z-10">
+        <Icon v-if="props.icon" :icon="props.icon" class="text-on-surface-variant/70"/>
+      </div>
+
       <input :id="props.id"
              :type="props.type"
              :aria-label="props.label"
              :name="props.name"
              placeholder=""
+             :class="{ 'pl-12': props.icon }"
              class="w-full h-14 leading-5 relative py-2 px-4 rounded border border-on-surface-variant/70 focus:border-primary outline-none focus:ring-0 peer"/>
-
       <label
-        class="absolute tracking-[.03125em] text-on-surface-variant/70 duration-300 transform px-1 -translate-y-7 scale-75 top-4 z-10 origin-[0] left-4 peer-focus:left-4 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7 peer-focus:bg-surface peer-focus:px-1 peer-invalid:text-error"
+        class="absolute tracking-[.03125em] text-on-surface-variant/70 duration-300 transform px-1 -translate-y-7 scale-75 top-4 z-10 origin-[0] peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-7 peer-focus:bg-surface peer-focus:px-1 peer-invalid:text-error"
+        :class="{
+          'left-12 peer-focus:left-12': props.icon,
+          'left-4 peer-focus:left-4': !props.icon
+        }"
         :for="props.name">
         {{ props.label }}
       </label>
     </div>
-
-    <!--
-
-    absolute tracking-[.03125em] bg-neutral-10 dark:bg-neutral-900
-    duration-300 transform px-1 -translate-y-7 scale-75 top-4 z-10 origin-[0] left-4
-    peer-focus:left-4 peer-focus:text-primary
-    peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75
-    peer-focus:-translate-y-7 peer-focus:bg-surface dark:peer-focus:bg-neutral-900
-     dark:peer-invalid:text-error-200
-
-    -->
 
     <p v-if="props.support_text" class="pt-1 px-4 text-xs tracking-[0.4px]">
       {{ props.support_text }}
@@ -34,6 +32,8 @@
 </template>
 
 <script setup lang="ts">
+
+import Icon from "@/components/material/Icon.vue";
 
 type AllowedTypes = "text" | "password" | "email" | "number";
 
